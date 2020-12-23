d2f62dfa6339036695cd5b7a425d2218ad7f7c10
Karthik V S <karthikvs888@gmail.com>
2020-12-23 18:48:37 +0530

    version-1.1

---
+++ src/test/java/Test/Test1.java
@@ -0,0 +1,14 @@
+package Test;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test1
+{
+	@Test
+	public void tc_01()
+	{
+		Reporter.log("GitHub",true);
+	}
+
+}
